d4a247fae8fc3eede88f2b42b2cffdb54927fba4
jeppe-t <jeppethylstrup@yahoo.dk>
2022-04-14 08:13:12 +0200

    Small changes mapper..

---
+++ src/main/java/dk/jt/githubactionsexample/data/mapper/DataMapper.java
@@ -0,0 +1,28 @@
+package dk.jt.githubactionsexample.data.mapper;
+
+import dk.jt.githubactionsexample.data.repository.CustomerRepository;
+import dk.jt.githubactionsexample.domain.model.Userz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataMapper implements CommandLineRunner {
+
+    @Autowired CustomerRepository customerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //Autowire some code here
+
+        Userz userz = new Userz();
+        userz.setName("Testuser");
+        customerRepository.save(userz);
+
+
+        //Small changes here for git action
+        System.out.println("Testing the action..");
+
+    }
+}
